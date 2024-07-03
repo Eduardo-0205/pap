9ebe4b20da7939545d7852250e6e63aa30968aeb
Eduardo-0205 <154480600+Eduardo-0205@users.noreply.github.com>
2024-07-04 00:41:00 +0100

    Add files via upload

---
+++ form.php
@@ -0,0 +1,26 @@
+<?php 
+$name = $_POST['name'];
+$visitor_email =  $_POST['email'];
+$message = $_POST['mensagem'];
+
+$email_from = 'eduardocreativecosupport@gmail.com';
+
+$email_subject = 'Nova mensagem';
+
+$email_body = "Nome:$name.\n".
+               "Email:$visitor_email.\n".
+                "Mensagem:$message.\n";
+
+$to = 'eduardocorrr05@gmail.com';
+
+$headers = "De: $email_from \r\n";
+
+$headers .= "Para: $visitor_email\r\n"; 
+
+mail($to,$email_subject,$email_body);
+
+header("Location: contactos.html");
+
+
+
+?>
